3287faa7ef7319612a87ff855c7fff37689b7c6b
Robson Arita <robsonkeniti@gmail.com>
2024-08-02 11:30:55 -0300

    remove dist

--- dist/core/service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Service = void 0;
-class Service {
-    constructor(postgresAdapter) {
-        this.postgresAdapter = postgresAdapter;
-    }
-}
-exports.Service = Service;
